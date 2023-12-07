80747851c3219616a27a3c40dec56b1a2dd52938
annebrandes <annebrandes1@gmail.com>
2023-12-07 00:34:24 -0500

    day 2, part 1

--- day-2/day2-part1.cpp
+++ day-2/day2-part1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
+#include <sstream>
 
 int main() {
     std::ifstream file("day2-input.txt");
@@ -11,9 +13,54 @@ int main() {
         return 1;
     }
 
+    int game_sum = 0;
+
     while (getline(file, line)) {
-        std::cout << line << std::endl;
+
+        std::istringstream iss(line);
+        std::string temp;
+        int game_num;
+        iss >> temp >> game_num;    
+
+        std::regex redRegex("(\\d+) red");
+        std::regex greenRegex("(\\d+) green");
+        std::regex blueRegex("(\\d+) blue");
+
+        std::smatch match;
+
+        int red = 0, green = 0, blue = 0;
+        std::string tempLine = line;
+
+        while (std::regex_search(tempLine, match, redRegex)) {
+            red = std::max(red, std::stoi(match[1].str()));
+            tempLine = match.suffix();
+        }
+        
+        tempLine = line;
+
+        while (std::regex_search(tempLine, match, greenRegex)) {
+            green = std::max(green, std::stoi(match[1].str()));
+            tempLine = match.suffix();
+        }
+        
+        tempLine = line;
+
+        while (std::regex_search(tempLine, match, blueRegex)) {
+            blue = std::max(blue, std::stoi(match[1].str()));
+            tempLine = match.suffix();
+        }
+
+        std::cout << "Red: " << red << ", Green: " << green << ", Blue: " << blue << std::endl;
+
+        if (red <= 12 && green <= 13 && blue <= 14) {
+            std::cout << "Game " << game_num << " is possible" << std::endl;
+            game_sum += game_num;
+        } else {
+            continue;
+        }
+
     }
 
-    return 0;
+    std::cout << "Game sum: " << game_sum << std::endl;
+    return game_sum;
 }
